b47306e0dd4a9d4cf91370440bd4271e00897d40
Ari Asulin <ari@newaer.com>
2015-02-15 16:48:41 -0700

    fix2

--- Processor/Account/ManageAccount.php
+++ Processor/Account/ManageAccount.php
@@ -151,8 +151,8 @@ class ManageAccount implements IExecutable, IBuildable, IRoutable
 	static function handleBuildStatic(IBuildRequest $Request) {
 		$RouteBuilder = new RouteBuilder($Request, new SiteMap());
 
-		$RouteBuilder->writeRoute('ANY ' . self::FORM_ACTION, __CLASS__, IRequest::MATCH_NO_SESSION);
-		$RouteBuilder->writeRoute('ANY ' . self::FORM_ACTION2, __CLASS__, IRequest::MATCH_NO_SESSION);
-		$RouteBuilder->writeRoute('ANY ' . self::FORM_ACTION3, __CLASS__, IRequest::MATCH_NO_SESSION);
+		$RouteBuilder->writeRoute('ANY ' . self::FORM_ACTION, __CLASS__);
+		$RouteBuilder->writeRoute('ANY ' . self::FORM_ACTION2, __CLASS__);
+		$RouteBuilder->writeRoute('ANY ' . self::FORM_ACTION3, __CLASS__);
 	}
 }
